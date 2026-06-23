784f5b6e40fcd14b6527f8da99f0c815c95331d5
mysmay722 <mysmay722@gmail.com>
2026-06-23 15:59:06 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 MAPA_IMAGENES = {
     "M4 Lizard": "assets/m4_lizard0.PNG",
     "G22 Flock": "assets/G22_Flock.PNG",
-    "M4 Flock": "assets/M4_Flock.PNG" 
-    "Desert Eagle Violet Flame": "assets/DE_Violet Flame.PNG"
-    "Val Joker": "assets/VAL_Joker.PNG"
-    "M4A1 Stainless": "assets/M4A1_Stainless.PNG"
-    "USP Ghosts": "assets/USP_Ghosts.PNG"
-    "M40 Disguise": "assets/M40_Disguise.PNG" # <-- Nueva línea
+    "M4 Flock": "assets/M4_Flock.PNG", 
+    "Desert Eagle Violet Flame": "assets/DE_Violet Flame.PNG",
+    "Val Joker": "assets/VAL_Joker.PNG",
+    "M4A1 Stainless": "assets/M4A1_Stainless.PNG",
+    "USP Ghosts": "assets/USP_Ghosts.PNG",
+    "M40 Disguise": "assets/M40_Disguise.PNG", # <-- Nueva línea
 }
 
