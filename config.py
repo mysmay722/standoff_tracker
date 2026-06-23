f8089c22497df45fa1ddf575ed4cd59a4dbbe0cc
mysmay722SO2 <mysmay722@gmail.com>
2026-06-23 13:40:15 -0600

    Corrigiendo ruta de imagen en config.py

--- config.py
+++ config.py
@@ -1,3 +1,25 @@
+  GNU nano 8.4                                                                       config.py                                                                                 
 MAPA_IMAGENES = {
     "M4 Lizard": "m4_lizard0.png",
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
