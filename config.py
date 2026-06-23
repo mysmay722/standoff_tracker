924f4b3bc688f20cf48684de4814585540f5bedc
mysmay722SO2 <mysmay722@gmail.com>
2026-06-23 15:27:30 -0600

    Añadiendo nuevas skins al catálogo

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
-  GNU nano 8.4                                                                       config.py                                                                                 
 MAPA_IMAGENES = {
-    "M4 Lizard": "m4_lizard0.png",
+    "M4 Lizard": "assets/m4_lizard0.PNG",
+    "G22 Flock": "assets/G22_Flock.PNG",
+    "M4 Flock": "assets/M4_Flock.PNG" 
+    "Desert Eagle Violet Flame": "assets/DE_Violet Flame.PNG"
+    "Val Joker": "assets/VAL_Joker.PNG"
+    "M4A1 Stainless": "assets/M4A1_Stainless.PNG"
+    "USP Ghosts": "assets/USP_Ghosts.PNG"
+    "M40 Disguise": "assets/M40_Disguise.PNG" # <-- Nueva línea
 }
-
-
 
 
 
